5ea0166106c39e8746c1a57f9a82fd032a4c02c5
Bradley Cranston <brad@cranstoninc.com>
2024-03-16 10:26:34 -0400

    wrote object methods

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import { companiesData } from "./companies-data";
+import {extraRecord} from "./extra-record";
+
+console.log("Extra",extraRecord)
 
 const before = document.getElementById("before");
 before.innerHTML = JSON.stringify(companiesData, null, 2);
 
-const manipulatedData = [1,2,3];
+//const manipulatedData = Object.values(extraRecord.fieldData.City);
+
+//extraRecord.fieldData.Country = "USA";
+//extraRecord.fieldData.CityState = `${extraRecord.fieldData.City}, ${extraRecord.fieldData.State}`
+//delete extraRecord.fieldData.Id;
+//const {City, State, Zip, ...rest} = extraRecord.fieldData;
+//console.log(rest);
+
+const manipulatedData = extraRecord.fieldData;
+
 
 //TODO: add the manipulated data array to the 'after' div.
 const after = document.getElementById("after");
 after.innerHTML = JSON.stringify(manipulatedData,null,2)
+
